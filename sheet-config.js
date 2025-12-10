7b9f389ad664cbc3a8df937221aae8d03d7dfe82
e993890-bit <e993890@gmail.com>
2025-12-10 20:47:41 +0800

    Update API URLs with specific query parameters

--- sheet-config.js
+++ sheet-config.js
@@ -2,13 +2,14 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbwMe5DEhkEjjKDVCmghV9YjZVK9E3KXhR8C5VM_l51zY2PJi9matAn6efd8NfHc2Yc/exec";
 
 // 登入 API
-const LOGIN_API = API_URL;
+const LOGIN_API = API_URL + "?type=login";
 
 // 上架資料寫入 API (POST)
 const WRITE_API = API_URL;
 
-// 主管報表 API（以後可擴充）
+// 報表 API
 const REPORT_API = API_URL + "?report=1";
+
 
 
 
